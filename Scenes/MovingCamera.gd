cac4f0cead2c858babd9e4821d0bf2f9fe0055c8
Carson Webster <CarsonWebster@hotmail.com>
2024-01-27 19:50:49 -0800

    Camera with silly npcs

---
+++ Scenes/MovingCamera.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var corrners = $Corrners
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(corrners)
+	print(corrners.get_children())
+	move_camera(corrners.get_children().pick_random().position)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func move_camera(pos: Vector2):
+	print("Moving camera to: ", pos)
+	var tween = get_tree().create_tween()
+	tween.tween_property($".", "position", pos, 5)
+	#tween.tween_callback($".".move_camera(corrners.get_children().pick_random().position))
+	tween.connect("finished", on_tween_finished)
+
+func on_tween_finished():
+	print("Tween done!")
+	await get_tree().create_timer(2).timeout
+	move_camera(corrners.get_children().pick_random().position)
---
+++ Scripts/VictimSpawner.gd
@@ -0,0 +1,37 @@
+extends Node
+
+@export var victim_scene: PackedScene
+@export var victims: Node2D
+@export var victim_spawn_timer_amount: float = 2.5
+
+@onready var spawn_timer = $Timer
+@onready var spawn_points = $SpawnPoints
+
+var possible_points
+var selected_spawn_point
+
+var rng = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	spawn_timer.start(victim_spawn_timer_amount)
+	#print("Spawn Points: ", spawn_points)
+	possible_points = spawn_points.get_children()
+	#print("Possible Points: ", possible_points) 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_timer_timeout():
+	spawn_victim()
+
+func spawn_victim():
+	selected_spawn_point = possible_points.pick_random()
+	#print("Selected Spawn ", selected_spawn_point)
+	var victim = victim_scene.instantiate()
+	victims.add_child(victim)
+	victim.position = selected_spawn_point.position
+	print("Spawned: ", victim, "at: ", victim.position)
